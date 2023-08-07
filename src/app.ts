bdb14ddba2157dac13b3cbb9e8f37835579607a4
Apodlubny <apodlubny@gmail.com>
2023-08-07 16:41:32 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -231,7 +231,7 @@ const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.openDoor(person.§getKey());
 
 house.comeIn(person);
   
